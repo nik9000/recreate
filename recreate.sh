d6b5aa9283fe39cacd9fbf811e04f1d5f13b1da8
Nik Everett <nik9000@gmail.com>
2018-09-18 10:43:33 -0400

    More

--- recreate.sh
+++ recreate.sh
@@ -5,13 +5,23 @@ set +o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-echo "Caching sudo privileges"
+header() {
+    printf "====================%-30s====================\n" "$1"
+}
+
+append() {
+    FILE="$1"
+    LINE="$2"
+    grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+}
+
+header "Caching sudo privileges"
 sudo echo "got 'em"
 
-echo "Creating ~/Bin"
+header "Creating ~/Bin"
 mkdir -p ~/Bin
 
-echo "Hacking backlight"
+header "Hacking backlight"
 cat <<__CONF | sudo tee /etc/X11/xorg.conf.d/10-backlight.conf
 Section "Device"
     Identifier  "Card0"
@@ -20,7 +30,7 @@ Section "Device"
 EndSection
 __CONF
 
-echo "Creating multi-monitor script"
+header "Creating multi-monitor script"
 cat <<__BASH | tee ~/Bin/multi-monitor
 #!/bin/bash
 
@@ -28,21 +38,47 @@ xrandr --output eDP1 --auto --output DP1 --auto --right-of eDP1
 __BASH
 chmod +x ~/Bin/multi-monitor
 
-echo "Install a nice font"
-sudo dnf install pcaro-hermit-fonts.noarch
+header "Add rpm fusion"
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo "Install window manager"
-sudo dnf install i3
+header "Update"
+sudo dnf update -y
 
-echo "Install background manager"
-sudo dnf install feh
+header "Install vim"
+sudo dnf install -y vim
 
-echo "Setting up config"
+header "Install a nice font"
+sudo dnf install -y pcaro-hermit-fonts.noarch
+
+header "Install window manager"
+sudo dnf install -y i3
+
+header "Install background manager"
+sudo dnf install -y feh
+
+header "Setting up config"
+mkdir -p ~/.config/i3
 cp $DIR/config/i3/config ~/.config/i3/config
+mkdir -p ~/.config/i3status
 cp $DIR/config/i3status/config ~/.config/i3status/config
 
-echo "Install VirtualBox"
-# Looks like this is in rpmfusion anyway
-# wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo rpm --import -
-# wget -q https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O- | sudo tee /etc/yum.repos.d/virtualbox.repo
-sudo dnv install VirtualBox
+header "Install VirtualBox"
+sudo dnf install -y VirtualBox
+
+header "Install VSCode"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install -y code
+
+header "Instal Javas"
+sudo dnf install -y java-1.8.0-openjdk-devel java-9-openjdk-devel java-10-openjdk-devel java-11-openjdk-devel
+cat <<__BASH | tee ~/.java_env
+export JAVA8_HOME=/usr/lib/jvm/java-1.8.0
+export JAVA9_HOME=/usr/lib/jvm/java-9
+export JAVA10_HOME=/usr/lib/jvm/java-10
+export JAVA11_HOME=/usr/lib/jvm/java-11
+export JAVA_HOME=\$JAVA10_HOME
+__BASH
+append ~/.bashrc "source ~/.java_env"
+
