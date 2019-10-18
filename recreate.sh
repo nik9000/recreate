bb8926955f4052496a6e316abb8e9f0f2e2f2de0
Nik Everett <nik9000@gmail.com>
2019-10-18 08:35:33 -0400

    Remove last we don't need

--- recreate.sh
+++ recreate.sh
@@ -25,32 +25,6 @@ sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.m
 sudo dnf install -y code
 code --help > /dev/null # run code to create the config files
 
-header "Install Javas"
-sudo dnf install -y jq
-install_java() {
-    VERSION=$1
-    echo "Downloading java $VERSION"
-    cd /tmp
-    [ -f openjdk${VERSION}.tar.gz ] || curl $(curl -s https://jvm-catalog.elastic.co/jdk/latest_openjdk_${VERSION}_linux | jq -r .url) > openjdk${VERSION}.tar.gz
-    tar -xf openjdk${VERSION}.tar.gz
-    sudo rm -rf /usr/lib/jvm/java-${VERSION}
-    sudo mv jdk-${VERSION}* /usr/lib/jvm/java-${VERSION}
-    sudo chown -R root:root /usr/lib/jvm/java-${VERSION}
-}
-sudo dnf install -y java-1.8.0-openjdk-devel
-install_java  9
-install_java 10
-install_java 11
-install_java 12
-cat <<__BASH | tee ~/.java_env
-export JAVA8_HOME=/usr/lib/jvm/java-1.8.0
-export JAVA9_HOME=/usr/lib/jvm/java-9
-export JAVA10_HOME=/usr/lib/jvm/java-10
-export JAVA11_HOME=/usr/lib/jvm/java-11
-export JAVA12_HOME=/usr/lib/jvm/java-12
-export JAVA_HOME=\$JAVA12_HOME
-__BASH
-append ~/.bashrc "source ~/.java_env"
 
 header "Install Slack"
 rpm -q slack || sudo dnf install -y https://downloads.slack-edge.com/linux_releases/slack-3.4.2-0.1.fc21.x86_64.rpm
