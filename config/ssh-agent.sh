9fddb7038a79f978ee4b85490810b121bf179328
Nik Everett <nik9000@gmail.com>
2019-11-11 13:46:20 -0500

    SSH

---
+++ config/ssh-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+  ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+  eval "$(<"$XDG_RUNTIME_DIR/ssh-agent.env")"
+fi
--- stage2.sh
+++ stage2.sh
@@ -19,16 +19,20 @@ install_aur() {
     cd Code/ArchUserRepository
     if [ -e $1 ]; then
       cd $1
-      git pull
+      git pull | grep 'Already up to date.' || rm $1*.pkg.tar.xz
     else
       git clone https://aur.archlinux.org/$1.git
       cd $1
     fi
-    makepkg -fs
+    ls $1*.pkg.tar.xz || makepkg
 BASH
-  ls $(pwd)/Code/ArchUserRepository/$1/$1*.pkg.tar.xz
   pacman -U --noconfirm $(pwd)/Code/ArchUserRepository/$1/$1*.pkg.tar.xz
 }
+
+append ~/.bashrc "export PATH=\$PATH:~/Bin"
+
+echo Update everything we already have
+pacman -Syu --noconfirm
 
 echo Setup basics
 install man base-devel git
@@ -46,7 +50,7 @@ install lightdm lightdm-gtk-greeter accountsservice
 systemctl enable lightdm
 
 echo Setup i3
-install xorg xorg-xinit i3-gaps i3blocks i3lock i3status xautolock feh xorg-xbacklight scrot rxvt-unicode dmenu
+install xorg xorg-xinit i3-gaps i3blocks i3lock i3status xautolock feh xorg-xbacklight scrot rxvt-unicode dmenu alsa-utils
 sudo -u manybubbles bash <<BASH
   set -xeo pipefail
   mkdir -p ~/.config/i3
@@ -64,3 +68,36 @@ sudo -u manybubbles bash <<BASH
   git config --global user.email "nik9000@gmail.com"
   git config --global alias.pr '!f() { git fetch elastic pull/$1/head:pr_$1; git checkout pr_$1; }; f'
 BASH
+
+echo Setup firefox
+install firefox
+
+echo Setup VSCode
+install code
+sudo -u manybubbles bash <<BASH
+  set -xeo pipefail
+  mkdir -p "~/.config/Code - OSS/User"
+  cp config/vscode/settings.json "~/.config/Code - OSS/User/settings.json"
+BASH
+sudo tee /etc/sysctl.d/max_user_watches.conf << CONF
+  fs.inotify.max_user_watches=524288
+CONF
+
+
+echo Setup slack
+install libappindicator-gtk3 # https://aur.archlinux.org/packages/slack-desktop/#comment-714391
+install libcurl-compat xdg-utils
+install_aur slack-desktop
+
+echo Setup docker
+install docker
+systemctl enable docker
+gpasswd -a manybubbles docker
+
+
+echo Setup ssh agent
+sudo -u manybubbles bash <<BASH
+  set -xeo pipefail
+  cp config/ssh-agent.sh .ssh-agent.sh
+BASH
+append ~/.bashrc "source .ssh-agent.sh"
